4ca79110cd4e0277350197a938ad30e50761b058
emmanuelokiche <fleepgeek@gmail.com>
2018-04-23 19:28:16 +0100

    Created Applicant and Company Model

--- accounts/models.py
+++ accounts/models.py
@@ -33,10 +33,10 @@ class UserManager(BaseUserManager):
         
 
 class User(AbstractBaseUser, PermissionsMixin):
-    full_name = models.CharField(_('full name'), max_length=30)
-    email = models.EmailField(_('email address'), unique=True)
-    is_staff = models.BooleanField(_('staff status'), default=False)
-    is_active = models.BooleanField(_('active'), default=True)
+    full_name   = models.CharField(_('full name'), max_length=30)
+    email       = models.EmailField(_('email address'), unique=True)
+    is_staff    = models.BooleanField(_('staff status'), default=False)
+    is_active   = models.BooleanField(_('active'), default=True)
     date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
 
     objects = UserManager()
@@ -47,7 +47,34 @@ class User(AbstractBaseUser, PermissionsMixin):
     class Meta:
         verbose_name = _('user')
         verbose_name_plural = _('users')
+    
+    def __str__(self): 
+        return self.email
 
     def clean(self):
         super().clean()
         self.email = self.__class__.objects.normalize_email(self.email)
+
+
+class Applicant(models.Model):
+    user            = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_of_birth   = models.DateField()
+    location        = models.CharField(_('Where do you Reside now'), max_length=20)
+    gender          = models.CharField(max_length=1)
+    about           = models.TextField(blank=True, null=True)
+    years_of_exp    = models.PositiveIntegerField(_('Years of Experience'))
+
+    def __str__(self):
+        return self.user
+
+
+class Company(models.Model):
+    name   = models.CharField(_('company name'), max_length=50)
+    description = models.TextField(_('describe your company'))
+    website     = models.URLField(_('company webite'))
+    country     = models.CharField(max_length=40)
+    state       = models.CharField(max_length=40)
+    address     = models.CharField(_('company address'), max_length=120)
+
+    def __str__(self):
+        return self.name
