2ae3fffe6155af6219419b8392aa433ff501b649
emmanuelokiche <fleepgeek@gmail.com>
2018-04-26 18:21:16 +0100

    crud for company job

--- jobs/urls.py
+++ jobs/urls.py
@@ -3,7 +3,11 @@ from django.urls import path
 from jobs import views
 
 urlpatterns = [
-    path('create/', views.JobCreate.as_view(), name='job_create'),
+    path('delete/<int:pk>/', views.JobDelete.as_view(extra_context={'title': 'Delete Job'}), name='job_delete'),
+    path('update/<int:pk>/', views.JobUpdate.as_view(extra_context={'title': 'Update Job'}), name='job_update'),
+    path('create/', views.JobCreate.as_view(extra_context={'title': 'Create Job'}), name='job_create'),
+    path('company/job/<int:pk>', views.JobsByCompanyDetail.as_view(), name='company_job_detail'),
+    path('company/', views.JobsByCompany.as_view(), name='company_jobs'),
     path('apply/', views.JobApply.as_view(), name='job_apply'),
     path('<slug:slug>/', views.JobDetail.as_view(), name='job'),
     path('', views.JobList.as_view(), name='jobs'),
--- jobs/views.py
+++ jobs/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import ListView, DetailView, CreateView, FormView, View
+from django.views.generic import View, ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib import messages
+from django.urls import reverse_lazy
 
 from .models import Job, Application
-from accounts.mixins import ApplicantRequiredMixin
+from accounts.mixins import ApplicantRequiredMixin, CompanyRequiredMixin
+from accounts.models import Company
+
 
 class JobList(ListView):
     model = Job
@@ -12,9 +15,50 @@ class JobDetail(DetailView):
     model = Job
     template_name='jobs/job_detail.html'
 
-class JobCreate(CreateView):
+
+class JobCreate(CompanyRequiredMixin, CreateView):
     model = Job
     fields = ('title', 'industry', 'job_type', 'min_qualification', 'years_of_exp', 'salary', 'description',)
+
+    def form_valid(self, form):
+        form.instance.company = self.request.user.company
+        return super().form_valid(form)
+
+
+class JobUpdate(CompanyRequiredMixin, UpdateView):
+    model = Job
+    fields = ('title', 'industry', 'job_type', 'min_qualification', 'years_of_exp', 'salary', 'description',)
+
+class JobDelete(CompanyRequiredMixin, DeleteView):
+    model = Job
+    success_url = reverse_lazy('jobs')
+
+
+class JobsByCompany(ListView):
+    template_name = 'companydashboard/job_list.html'
+    company = None
+    def get_queryset(self):
+        self.company = self.request.user.company
+        jobs = Job.objects.filter(company=self.company)
+        return jobs
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['company'] = self.company
+        return context
+
+class JobsByCompanyDetail(CompanyRequiredMixin, DetailView):
+    model = Job
+    template_name = 'companydashboard/job_detail.html'
+
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        applications = Application.objects.filter(job=self.get_object())
+        context['applications'] = applications
+        return context
+    
+
 
 class JobApply(ApplicantRequiredMixin, View):
     def get(self, request, *args, **kwargs):
